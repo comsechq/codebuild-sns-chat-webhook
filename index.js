f961b8d01433a26c03e896309d4efb73436aef57
Phil Higgins <phil.higgins@comsechq.com>
2021-07-29 15:06:41 +0100

    Changes to output messages from CodeBuild also

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ var https = require('https');
 var util = require('util');
 
 exports.handler = function(event, context) {
-    console.log(JSON.stringify(event, null, 2));
+	console.log(JSON.stringify(event, null, 2));
     console.log('From SNS:', event.Records[0].Sns.Message);
 
     var message = event.Records[0].Sns.Message;
 	var jsonMessage = JSON.parse(message);
 	
 	if(message.includes('approval')) {
-		
-      message = `<${jsonMessage.consoleLink}|*[${jsonMessage.approval.pipelineName}]*> pipeline requires approval:
+			
+			message = `<${jsonMessage.consoleLink}|*[${jsonMessage.approval.pipelineName}]*> pipeline requires approval:
 
 Stage  :  ${jsonMessage.approval.stageName}
 Action :  ${jsonMessage.approval.actionName}
@@ -18,15 +18,29 @@ Action :  ${jsonMessage.approval.actionName}
 Please click <${jsonMessage.approval.approvalReviewLink}|here> to approve or refuse.
 
 `;
-    } else {
-		
-	  var pipelineUrl = `https://console.aws.amazon.com/codesuite/codepipeline/pipelines/${jsonMessage.detail.pipeline}`;
-      message = `<${pipelineUrl}/view?region=${jsonMessage.region}|*[${jsonMessage.detail.pipeline}]*> pipeline has entered the state ${jsonMessage.detail.state}
+	} else {
+		if(jsonMessage.source.includes('aws.codepipeline')) {
+			var pipelineUrl = `https://console.aws.amazon.com/codesuite/codepipeline/pipelines/${jsonMessage.detail.pipeline}`;
+			
+			message = `<${pipelineUrl}/view?region=${jsonMessage.region}|*[${jsonMessage.detail.pipeline}]*> pipeline has entered the state ${jsonMessage.detail.state}
 
 Execution ID: <${pipelineUrl}/executions/${jsonMessage.detail['execution-id']}/timeline?region=${jsonMessage.region}|${jsonMessage.detail['execution-id']}>
 
 `;
-	}
+		}
+		else {
+			var buildUrl = `https://console.aws.amazon.com/codesuite/codebuild/projects/${jsonMessage.detail['project-name']}`;
+			var buildId = jsonMessage.detail['build-id'];
+		
+			buildId = buildId.slice(buildId.lastIndexOf('/') + 1);
+		
+			message = `<${buildUrl}/details?region=${jsonMessage.region}|*[${jsonMessage.detail['project-name']}]*> build has entered the state ${jsonMessage.detail['build-status']}
+
+Build ID: <${buildUrl}/build/${buildId}?region=${jsonMessage.region}|${buildId}>
+
+`;
+		}
+	}		
     
     var postData = {
         "text": message
